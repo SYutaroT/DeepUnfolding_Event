26e82a9d583e796ec0bf4f2c50e67d85cd99ea7a
SYutaroT <sachyand0515sachy@gmail.com>
2024-05-30 13:01:12 +0900

    DETCの機能を削った

--- NonLinier/Zhao_model1/DETC.py
+++ NonLinier/Zhao_model1/DETC.py
@@ -128,31 +128,6 @@ class CSTR(nn.Module):  # 深層展開のメインクラス
         self.datasize = 32
 # ?----------------------------------------------------------------関数
 
-    def f(self, x, u):  # 4次のルンゲクッタ法による離散化
-        h = self.h
-        Rx = torch.zeros(2, 1, dtype=torch.double).to(self.device)
-        x1 = x[0]
-        x2 = x[1]
-
-        k1_x1 = parameter.x1_dot(x1, x2, u)
-        k1_x2 = parameter.x2_dot(x1, x2, u)
-
-        k2_x1 = parameter.x1_dot(x1, x2 + h / 2, u)
-        k2_x2 = parameter.x2_dot(x1, x2 + h / 2, u)
-
-        k3_x1 = parameter.x1_dot(x1, x2 + h / 2, u)
-        k3_x2 = parameter.x2_dot(x1, x2 + h / 2, u)
-
-        k4_x1 = parameter.x1_dot(x1, x2 + h, u)
-        k4_x2 = parameter.x2_dot(x1, x2 + h, u)
-
-        x1_dot_avg = (k1_x1 + 2 * k2_x1 + 2 * k3_x1 + k4_x1) / 6
-        x2_dot_avg = (k1_x2 + 2 * k2_x2 + 2 * k3_x2 + k4_x2) / 6
-
-        Rx[0] = x1 + h * x1_dot_avg
-        Rx[1] = x2 + h * x2_dot_avg
-        return Rx
-
     def phi(self, x, Ix):  # !柔軟なトリガ
         Rx = torch.zeros(2*self.N, 1, dtype=torch.double).to(self.device)
         for i in range(0, self.N):
@@ -211,6 +186,7 @@ class CSTR(nn.Module):  # 深層展開のメインクラス
 
 
 # ?----------------------------------------------------------------メイン
+
 
     def utrain(self):  # !最重要、ここで訓練
         torch.autograd.set_detect_anomaly(True)
@@ -310,84 +286,8 @@ cstr = CSTR(T, N, epoch, device, imgname)
 
 # ?----------------------------------------------------------------関数
 
-
-def f(x, u, h):
-    Rx = torch.zeros(2, 1, dtype=torch.double).to(device)
-    x1 = x[0]
-    x2 = x[1]
-
-    k1_x1 = parameter.x1_dot(x1, x2, u)
-    k1_x2 = parameter.x2_dot(x1, x2, u)
-
-    k2_x1 = parameter.x1_dot(x1, x2 + h / 2, u)
-    k2_x2 = parameter.x2_dot(x1, x2 + h / 2, u)
-    k3_x1 = parameter.x1_dot(x1, x2 + h / 2, u)
-    k3_x2 = parameter.x2_dot(x1, x2 + h / 2, u)
-    k4_x1 = parameter.x1_dot(x1, x2 + h, u)
-    k4_x2 = parameter.x2_dot(x1, x2 + h, u)
-
-    x1_dot_avg = (k1_x1 + 2 * k2_x1 + 2 * k3_x1 + k4_x1) / 6
-    x2_dot_avg = (k1_x2 + 2 * k2_x2 + 2 * k3_x2 + k4_x2) / 6
-    Rx[0] = x1 + h * x1_dot_avg
-    Rx[1] = x2 + h * x2_dot_avg
-    return Rx
-
-
 def J(x, u, Q, R):
     return x.T @ Q @ x + u.T @ R @ u
-
-
-def calculate_cost(w, T, N, x0, Q, R, Qf, lam, K, h, Name, No):
-    x = torch.zeros(2, T, dtype=torch.double).to(device)
-    x_hat = torch.zeros((N, 1), dtype=torch.double).to(device)
-    u = torch.zeros(1, T-1, dtype=torch.double).to(device)
-    mark = torch.zeros(1, T-1, dtype=torch.double).to(device)
-    cost = torch.zeros(1, dtype=torch.double).to(device)
-    cost2 = torch.zeros(1, dtype=torch.double).to(device)
-    mark2 = torch.zeros(1, T-1, dtype=torch.double).to(device)
-    Rx = torch.zeros(4, 1, dtype=torch.double).to(device)
-    Rx_data = torch.zeros(1, T-1, dtype=torch.double).to(device)
-    x_t = torch.zeros(2, T, dtype=torch.double).to(device)
-    u_t = torch.zeros(1, T-1, dtype=torch.double).to(device)
-    cnt = 0
-    for i in range(0, N):
-        x[i, 0] = x0[i]
-        x_hat[i, 0] = x0[i]
-        x_t[i, 0] = x0[i]
-    for i in range(0, T-1):
-        if i == 0:
-            mark[:, i] = 1
-            Rx_data[:, i] = 1
-        else:
-            Rx[0] = x[0, [i]]
-            Rx[1] = x[1, [i]]
-            Rx[2] = x_hat[0, [0]]
-            Rx[3] = x_hat[1, [0]]
-            if 0 < Rx.T@L2@Rx+L1@Rx+L0:
-                x_hat[:, [0]] = x[:, [i]]
-                mark[:, i] = 1
-                cnt += 1
-            else:
-                mark[:, i] = 0
-            Rx_data[:, i] = torch.sigmoid(Rx.T@L2@Rx+L1@Rx+L0)
-        u[:, [i]] = K@x_hat
-        x[:, [i+1]] = f(x[:, [i]], u[:, [i]], h)+w[:, [i]]
-    for i in range(0, T-1):
-        cost = cost + x[:, i].T@Q@x[:, i]+u[:, i]@R@u[:, i]+lam*mark[:, i]
-
-    cost = cost + x[:, T-1].T@Qf@x[:, T-1]
-
-    imgname = Name+"_ev"+str(No)
-    for i in range(T-1):
-        u_t[:, i] = K@x_t[:, i]
-        for j in range(N):
-            x_t[j, i+1] = f(x_t[:, i], u_t[:, i], h)[j]+w[j, i]
-        mark2[:, i] = 1
-    for i in range(0, T-1):
-        cost2 += x_t[:, i].T@Q@x_t[:, i]+u_t[:, i]@R@u_t[:, i]+lam*mark2[:, i]
-    cost2 += x_t[:, T-1].T@Qf@x_t[:, T-1]
-
-    return cost.item(), mark.sum().item(), cost2.item()
 
 
 # ?----------------------------------------------------------------トレーニング
@@ -401,88 +301,16 @@ cstr.batchSize = Batch
 cstr.datasize = datasize
 cstr.utrain()
 # ?----------------------------------------------------------------変数
-x = torch.zeros((N, T), dtype=torch.double).to(device)
-x_hat = torch.zeros((N, T-1), dtype=torch.double).to(device)
-u = torch.zeros(1, T-1, dtype=torch.double).to(device)
-mark = torch.zeros(1, T-1, dtype=torch.double).to(device)
 K = cstr.K.clone()
-delta = cstr.delta.clone()
-Rx = torch.zeros(4, 1, dtype=torch.double).to(device)
-Rx_data = torch.zeros(1, T-1, dtype=torch.double).to(device)
-cnt = 0  # 更新回数
-# ?----------------------------------------------------------------
 L2 = cstr.L.clone()
 L1 = cstr.M.clone()
 L0 = cstr.Mo.clone()
-Q = cstr.Q.clone()
-Qf = cstr.Qf.clone()
-R = cstr.R.clone()
-best = cstr.best
+
 print("K", K)
 print("L2", L2)
 print("L1", L1)
 print("L0", L0)
 # ?----------------------------------------------------------------データの保存
-
-
-if not os.path.exists(directory):
-    os.makedirs(directory)
-with open(data, "w") as file:
-    file.write("Title:\n")
-    file.write(f"{imgname}\n")
-with open(data, "a") as file:  # 追加モード
-    file.write("\nL0:\n")
-    for value in L0:
-        file.write(f"{value}\n")
-with open(data, "a") as file:  # 追加モード
-    file.write("\nL1:\n")
-    for value in L1:
-        file.write(f"{value}\n")
-with open(data, "a") as file:  # 追加モード
-    file.write("\nL2:\n")
-    for value in L2:
-        file.write(f"{value}\n")
-with open(data, "a") as file:  # 追加モード
-    file.write("\nK:\n")
-    for value in K:
-        file.write(f"{value}\n")
-# ?----------------------------------------------------------------シミュレーション
-counter = 0
-total_cost = 0
-total_cnt = 0
-w_list = []
-
-for i in range(parameter.evolution):
-    seedinit(i)
-    w = cstr.w_for_evaluation(T-1)
-    w_list.append(w)
-
-for w in w_list:
-    counter += 1
-    cost,  cnt, cost2 = calculate_cost(
-        w, T, N, x0, Q, R, Qf, lam, K, h, imgname, counter)
-
-    with open(data, "a") as file:  # 追加モード
-        file.write("cost_ev"+str(counter)+"\n")
-        file.write(f"{cost}\n")
-    with open(data, "a") as file:  # 追加モード
-        file.write("cost2_ev"+str(counter)+"\n")
-        file.write(f"{cost2}\n")
-    with open(data, "a") as file:
-        file.write("cnt_ev"+str(counter)+"\n")
-        file.write(f"{cnt}\n")
-
-    total_cost += cost
-    total_cnt += cnt
-average_cost = total_cost / counter
-average_cnt = total_cnt / counter
-
-with open(data, "a") as file:  # 追加モード
-    file.write("cost_AVE\n")
-    file.write(f"{average_cost}\n")
-with open(data, "a") as file:
-    file.write("cnt_AVE\n")
-    file.write(f"{average_cnt}\n")
 parameters_content = f"""# Generated Parameters
 T= {T}
 L2 = {L2.tolist()}
@@ -495,7 +323,7 @@ parameters_file_path = imgname+"result.py"
 with open(parameters_file_path, "w") as file:
     file.write(parameters_content)
 # ?----------------------------------------------------------------時間
-time.sleep(2)  # 例として2秒間の遅延
+time.sleep(2)  # 2秒間の遅延
 
 # 処理終了後の時刻
 end_time = time.time()
